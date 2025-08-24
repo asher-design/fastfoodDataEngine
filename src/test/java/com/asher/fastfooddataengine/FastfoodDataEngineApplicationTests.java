70e90b4c80b48a177e22e556540b3d7f70945438
yangfulin <yfl@bupt.edu.cn>
2025-08-24 12:56:18 +0800

    Initial commit

---
+++ src/test/java/com/asher/fastfooddataengine/FastfoodDataEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asher.fastfooddataengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodDataEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
